e7654ecf5828dcce78ad1bd925a425a9725e3781
ZalanFazal <90651625+ZalanFazal@users.noreply.github.com>
2024-08-20 07:43:54 +0000

    js-2

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 console.log("zalan");
+//global and local scope in javascript
+// Scope in JavaScript refers to the current context of code, which determines the accessibility of variables to JavaScript. The two types of scope are local and global: Global variables are those declared outside of a block. Local variables are those declared inside of a block.
+
+var a=1;
+let b=2;
+const c = 4;
+console.log(a)
+console.log(b);
+console.log(c);
